f4489b265ccca5964f2df538a1c123709bc7a2c9
DESKTOP-4V50L6N <balajipandu64@gmail.com>
2022-03-17 17:15:13 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+def balu(request):
+    return HttpResponse('<marquee><h1>balu is a graduate<h1\><marquee\>')
+
+
+# Create your views here.
